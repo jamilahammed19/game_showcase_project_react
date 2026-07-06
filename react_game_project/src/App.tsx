7cb694ed4ed2205632a4ec928d3d3da61f934dba
jamilahammed19 <jamilahammed19@gmail.com>
2026-07-06 12:23:30 +0600

    Chakra UI install

--- react_game_project/src/App.tsx
+++ react_game_project/src/App.tsx
@@ -1,8 +1,20 @@
+import { Button, HStack } from "@chakra-ui/react";
+import { RiArrowRightLine, RiMailLine } from "react-icons/ri"
 import "./App.css";
 
 function App() {
-
-  return <></>;
+  return (
+    <>
+      <HStack>
+        <Button colorPalette="teal" variant="solid">
+          <RiMailLine /> Email
+        </Button>
+        <Button colorPalette="teal" variant="outline">
+          Call us <RiArrowRightLine />
+        </Button>
+      </HStack>
+    </>
+  );
 }
 
 export default App;
